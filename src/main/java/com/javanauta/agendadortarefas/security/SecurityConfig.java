3ee0c1954c60fc2ab5c05134be91a8f6f4b4640a
Sabrina <sabrinavitreis@gmail.com>
2026-07-10 21:50:46 -0300

    correção de apontamentos

--- src/main/java/com/javanauta/agendadortarefas/security/SecurityConfig.java
+++ src/main/java/com/javanauta/agendadortarefas/security/SecurityConfig.java
@@ -22,11 +22,11 @@ public class SecurityConfig {
 
     // Instâncias de JwtUtil e UserDetailsService injetadas pelo Spring
     private final JwtUtil jwtUtil;
-    private final UserDetailsService userDetailsService;
+    private final UserDetailsServiceImpl userDetailsService;
 
     // Construtor para injeção de dependências de JwtUtil e UserDetailsService
     @Autowired
-    public SecurityConfig(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+    public SecurityConfig(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
     }
