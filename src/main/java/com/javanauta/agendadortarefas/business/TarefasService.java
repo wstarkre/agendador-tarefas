c2151bcca4d1282a9f9b20447737e69a43e6bb78
Sabrina <sabrinavitreis@gmail.com>
2026-09-16 23:58:49 -0300

    add dockerfile e alterações necessárias

--- src/main/java/com/javanauta/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/javanauta/agendadortarefas/business/TarefasService.java
@@ -1,6 +1,6 @@
 package com.javanauta.agendadortarefas.business;
 
-import com.javanauta.agendadortarefas.business.dto.TarefasDTO;
+import com.javanauta.agendadortarefas.business.dto.TarefasDTORecord;
 import com.javanauta.agendadortarefas.business.mapper.TarefaUpdateConverter;
 import com.javanauta.agendadortarefas.business.mapper.TarefasConverter;
 import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
@@ -23,31 +23,31 @@ public class TarefasService {
     private final JwtUtil jwtUtil;
     private final TarefaUpdateConverter tarefaUpdateConverter;
 
-    public TarefasDTO gravarTarefa(String token, TarefasDTO dto) {
+    public TarefasDTORecord gravarTarefa(String token, TarefasDTORecord dto) {
         String email = jwtUtil.extrairEmailToken(token.substring(7));
 
-        dto.setDataCriacao(LocalDateTime.now());
-        dto.setStatusNotificacaoEnum(StatusNotificacaoEnum.PENDENTE);
-        dto.setEmailUsuario(email);
+        TarefasDTORecord dtoFinal = new TarefasDTORecord(null, dto.nomeTarefa(),
+                dto.descricao(), LocalDateTime.now(), dto.dataEvento(), email, null,
+                StatusNotificacaoEnum.PENDENTE);
         TarefasEntity entity = tarefaConverter.paraTarefaEntity(dto);
 
         return tarefaConverter.paraTarefaDTO(
                 tarefasRepository.save(entity));
     }
 
-    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
-        return tarefaConverter.paraListaTarefasDTO(
-                tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
-
+    public List<TarefasDTORecord> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
+        return tarefaConverter.paraListaTarefasDTORecord(
+                tarefasRepository.findByDataEventoBetweenAndStatusNotificacaoEnum(
+                        dataInicial, dataFinal, StatusNotificacaoEnum.PENDENTE));
 
     }
 
-    public List<TarefasDTO> buscaTarefasPorEmail(String token) {
+    public List<TarefasDTORecord> buscaTarefasPorEmail(String token) {
 
         String email = jwtUtil.extrairEmailToken(token.substring(7));
         List<TarefasEntity> ListaTarefas = tarefasRepository.findByEmailUsuario(email);
 
-        return tarefaConverter.paraListaTarefasDTO(ListaTarefas);
+        return tarefaConverter.paraListaTarefasDTORecord(ListaTarefas);
     }
 
     public void deletaTarefaPorId(String id) {
@@ -59,7 +59,7 @@ public class TarefasService {
         }
     }
 
-    public TarefasDTO alteraStatus(StatusNotificacaoEnum status, String id) {
+    public TarefasDTORecord alteraStatus(StatusNotificacaoEnum status, String id) {
         try {
             TarefasEntity entity = tarefasRepository.findById(id).
                     orElseThrow(() -> new ResourceNotFoundException("Tarefa não encontrada " + id));
@@ -70,7 +70,7 @@ public class TarefasService {
         }
     }
 
-    public TarefasDTO updateTarefas(TarefasDTO dto, String id) {
+    public TarefasDTORecord updateTarefas(TarefasDTORecord dto, String id) {
         try {
             TarefasEntity entity = tarefasRepository.findById(id).
                     orElseThrow(() -> new ResourceNotFoundException("Tarefa não encontrada " + id));
--- src/main/java/com/javanauta/agendadortarefas/business/dto/TarefasDTORecord.java
+++ src/main/java/com/javanauta/agendadortarefas/business/dto/TarefasDTORecord.java
@@ -2,27 +2,20 @@ package com.javanauta.agendadortarefas.business.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
-import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class TarefasDTO {
-
-    private String id;
-    private String nomeTarefa;
-    private String descricao;
+public record TarefasDTORecord(
+    String id,
+    String nomeTarefa,
+    String descricao,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss" )
-    private LocalDateTime dataCriacao;
+    LocalDateTime dataCriacao,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss" )
-    private LocalDateTime dataEvento;
-    private String emailUsuario;
+    LocalDateTime dataEvento,
+    String emailUsuario,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss" )
-    private LocalDateTime dataAlteracao;
-    private StatusNotificacaoEnum statusNotificacaoEnum;
+    LocalDateTime dataAlteracao,
+        StatusNotificacaoEnum statusNotificacaoEnum){
 
 }
